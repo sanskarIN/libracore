74cae1a35a8593f60dd21a29fa26fa50a8344625
Sanskar <sanskarin@outlook.in>
2026-08-19 19:21:59 +0530

    feat: enable notification outbox scheduling

---
+++ backend/src/main/java/com/sanskar/libracore/notification/NotificationSchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.sanskar.libracore.notification;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class NotificationSchedulingConfig {
+}
