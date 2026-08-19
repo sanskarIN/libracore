e0e26a7a687d4cd0e25ea022facaef5c84b3c5cc
Sanskar <sanskarin@outlook.in>
2026-08-19 19:08:34 +0530

    feat: add backend application entry point

---
+++ backend/src/main/java/com/sanskar/libracore/LibraCoreApplication.java
@@ -0,0 +1,11 @@
+package com.sanskar.libracore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraCoreApplication.class, args);
+    }
+}
